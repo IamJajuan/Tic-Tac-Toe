f378f9952d0701d5a120d14cc46061e7ff27b84f
jajua <jajuanleehall1998@gmail.com>
2021-09-03 13:38:58 -0400

    Update leave game functionality

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
   HashRouter,
--- front-end/src/pages/TwoPlayer.jsx
+++ front-end/src/pages/TwoPlayer.jsx
@@ -106,6 +106,7 @@ export default function TwoPlayer() {
 
             
               setMsg(msg)
+              socketRef.current.close()
 
                   
          })
